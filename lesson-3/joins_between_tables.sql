27324d039a5a5ca00b55b13bff4f4069bec6316e
mahmudjon <mahmudjonfoziljonov1@gmail.com>
2026-03-11 11:55:46 +0500

    lesson-2

--- lesson-3/joins_between_tables.sql
+++ lesson-3/joins_between_tables.sql
@@ -17,7 +17,24 @@ FROM weather wt
 
 SELECT *
 FROM weather
-         LEFT OUTER JOIN cities ON city = name; --This query is called a left outer join
+         LEFT OUTER JOIN cities ON city = name;
+--This query is called a left outer join
 -- because the table mentioned on the left of the join operator will have each of its rows in the output at least once,
 -- whereas the table on the right will only have those rows output that match some row of the left table
 
+SELECT *
+FROM weather
+WHERE prcp > 0.00
+  AND city = 'Tashkent';
+
+
+SELECT DISTINCT city
+FROM weather
+ORDER BY city;
+
+SELECT city, temp_lo, temp_hi, (temp_hi + temp_lo) / 2 temp_avg
+FROM weather;
+
+SELECT weather.city, weather.temp_hi, weather.date
+FROM weather
+ORDER BY city desc, date
